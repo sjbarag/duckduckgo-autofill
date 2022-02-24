cbda0d2721c07166ab5c6bac84ce716b70acfdaf
Shane Osbourne <shane.osbourne8@gmail.com>
2022-02-24 17:52:38 +0000

    tests for interfaceprototype

---
+++ src/DeviceInterface/InterfacePrototype.test.js
@@ -0,0 +1,41 @@
+const InterfacePrototype = require('./InterfacePrototype')
+describe('InterfacePrototype', () => {
+    it('storeLocalData should handle duplicate emails', () => {
+        const int = new InterfacePrototype()
+        int.storeLocalAddresses({
+            privateAddress: 'gergoiuh539845',
+            personalAddress: 'shane'
+        })
+        int.storeLocalData({
+            identities: [{
+                id: '01',
+                emailAddress: 'shane@duck.com',
+                title: 'Main'
+            }],
+            credentials: [],
+            creditCards: [],
+            serializedInputContext: ''
+        })
+        // this should result in 2 identities - 2 from email and the other one matches, so is merged
+        expect(int.getLocalIdentities().length).toBe(2)
+    })
+    it('storeLocalData should handle none-duplicates', () => {
+        const int = new InterfacePrototype()
+        int.storeLocalAddresses({
+            privateAddress: 'gergoiuh539845',
+            personalAddress: 'shane'
+        })
+        int.storeLocalData({
+            identities: [{
+                id: '01',
+                emailAddress: 'shane@example.com',
+                title: 'Main'
+            }],
+            credentials: [],
+            creditCards: [],
+            serializedInputContext: ''
+        })
+        // this should result in 3 identities - 2 from email and the other 1
+        expect(int.getLocalIdentities().length).toBe(3)
+    })
+})
